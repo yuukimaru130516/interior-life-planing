ce61f4a5df55b5e9f55be184b54634a54926f929
yuukimaru <yuuk130516@gmail.com>
2022-02-13 18:26:05 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'お部屋改造計画' });
+});
+
+module.exports = router;
